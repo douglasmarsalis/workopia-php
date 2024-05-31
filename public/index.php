5bcfaeaaa09d49e82001d5a9e42a9cbfcc47c582
Douglas <japanmarsalis@gmail.com>
2024-05-31 15:02:20 +0900

    Create Basic Router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php
 require '../helpers.php';
 
-loadView('home');
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+//inspectAndDie($uri);
+// localhost:8000 -> will see string(1) "/"
+// localhost:8000/listings -> will see string(9) "/listings"
+
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else {
+    require basePath($routes['404']);
+}
 
