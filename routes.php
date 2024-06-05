529c9ca9577e2de57b8cbf1fda3c3f96048a381b
Douglas <japanmarsalis@gmail.com>
2024-06-05 14:42:58 +0900

    Seperate Router Files

---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+//inspectAndDie($uri);
+// localhost:8000 -> will see string(1) "/"
+// localhost:8000/listings -> will see string(9) "/listings"
